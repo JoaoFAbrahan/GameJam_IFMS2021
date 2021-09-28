09d736935bcebdb47992f217c82bcee4fc257df3
João Felipe Abrahan <joaofelipeabrahan@hotmail.com>
2021-09-28 15:07:58 -0400

    adicionado item coletavel

---
+++ Assets/Probs/Items/Life_Item/Code/CODE_Life_Item.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _on_Life_Item_body_entered(body):
+	$AnimationPlayer.play(("Collected"));
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Collected":
+		queue_free();
