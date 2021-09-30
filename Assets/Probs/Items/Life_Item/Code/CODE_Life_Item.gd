2ea6639e60419193787a0f0e069bafd95297ba40
Anderson Luiz <andersonluizbazzotti@gmail.com>
2021-09-30 18:40:45 -0400

    Adicionado SFX Life Item

--- Assets/Probs/Items/Life_Item/Code/CODE_Life_Item.gd
+++ Assets/Probs/Items/Life_Item/Code/CODE_Life_Item.gd
@@ -3,6 +3,7 @@ extends Area2D
 func _on_Life_Item_body_entered(body):
 	body.Heal(12);
 	body.HealthBar.Health_Update(body.health, -20);
+	$FruitCollect_SFX.play();
 	$AnimationPlayer.play(("Collected"));
 
 func _on_AnimationPlayer_animation_finished(anim_name):
