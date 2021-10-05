3b38505ce29bba5a07d9f08d7192232a8b77f8c3
Andre <andreluizrodrigues14@gmail.com>
2021-10-05 01:31:12 -0400

    Adicionado fullscreen #1
    Implementado a opção de deixar em tela cheia

--- User Interface/Scripts/Menu.gd
+++ User Interface/Scripts/Menu.gd
@@ -6,10 +6,15 @@ func _on_Button_pressed():
 
 func _on_Config_pressed():
 	get_node("Sound").show()
-	AudioServer.generate_bus_layout() 
+	AudioServer.generate_bus_layout()
+	 
 func _on_Button3_pressed():
 	get_tree().quit()
 
 
 func _on_HSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+
+
+func _on_FullScreen_pressed():
+	OS.window_fullscreen = !OS.window_fullscreen
