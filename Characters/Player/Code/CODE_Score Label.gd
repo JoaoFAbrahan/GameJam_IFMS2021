10d62f5b9cef86fbda7d7fb394deede119944f4a
João Felipe Abrahan <joaofelipeabrahan@hotmail.com>
2021-10-06 14:06:00 -0400

    Correção de Texto e adicionado Level_00
    #3

--- Characters/Player/Code/CODE_Score Label.gd
+++ Characters/Player/Code/CODE_Score Label.gd
@@ -2,5 +2,5 @@ extends Label
 
 #### Passa as informações para a UI
 func _on_Main_Player_onCheckScore(_myScore, _levelScore):
-	text = "Score Need: " + String(_levelScore);
+	text = "Score Needed: " + String(_levelScore);
 	$"MyScore Label".text = "Your Score: " + String(_myScore);
