956f9e66c92a0ba0e9d015fced0229f6dff6f2f3
João Felipe Abrahan <joaofelipeabrahan@hotmail.com>
2021-10-05 23:34:28 -0400

    .4: Auto stash before merge of "v0.4" and "origin/v0.4"

--- Levels/Maps/Level_01/Code/CODE_LEVEL_Level_01.gd
+++ Levels/Maps/Level_01/Code/CODE_LEVEL_Level_01.gd
@@ -4,8 +4,7 @@ var soundController = true;
 
 func _process(delta):
 	if player.Life_Status() == false:
-		$"World Components/Main_Player/Level01_Soundtrack".stop();
-		#$"World Components/Main_Player/GameOver/GameOver_Controller".show();
+		$"World Components/Main_Player/Level_Soundtrack".stop();
 		if soundController:
 			$"World Components/Main_Player/GameOver_Soundtrack".play();
 			soundController = false;
