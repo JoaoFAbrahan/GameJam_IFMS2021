a28f62fe02b79f89b2634603dbdbb3c7d5329eb3
Andre <andreluizrodrigues14@gmail.com>
2021-10-06 01:44:51 -0400

    Alterado menu principal e menu da primeira fase #1
    adicionado nova imagem de fundo do menu principal, removido botão de som ingame e implementado opção de volume na primeira fase

--- User Interface/Scripts/Interface.gd
+++ User Interface/Scripts/Interface.gd
@@ -19,6 +19,17 @@ func _on_MenuPrincipal_pressed():
 	get_tree().paused = false
 	get_tree().change_scene("res://User Interface/Menu/Menu.tscn")
 
+func _on_HSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+
+
+func _on_FullScreen_pressed():
+	OS.window_fullscreen = !OS.window_fullscreen
+	
 func _on_Sair_pressed():
 	get_tree().quit()
 
+
+
+func _on_Sound_pressed():
+	pass # Replace with function body.
