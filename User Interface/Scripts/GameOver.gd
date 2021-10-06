a74356b8dab1db9525c6300257c78ff9e3084f35
Andre <andreluizrodrigues14@gmail.com>
2021-10-05 22:26:21 -0400

    Menu gameover funcional #1
    Removido node hide

--- User Interface/Scripts/GameOver.gd
+++ User Interface/Scripts/GameOver.gd
@@ -3,7 +3,6 @@ extends Control
 # Reinicia a fase
 func _on_Reiniciar_pressed():
 	get_tree().reload_current_scene()
-	get_node("GameOver").hide()
 
 # Volta ao menu principal
 func _on_MenuPrincipal_pressed():
