0cc372dc1ab28b34a5f371e4482c651051840751
Andre <andreluizrodrigues14@gmail.com>
2021-10-06 15:45:36 -0400

    Alterado menu pausa level 1 #1
    adicionado som e fullscreen

--- User Interface/Scripts/Interface.gd
+++ User Interface/Scripts/Interface.gd
@@ -19,17 +19,15 @@ func _on_MenuPrincipal_pressed():
 	get_tree().paused = false
 	get_tree().change_scene("res://User Interface/Menu/Menu.tscn")
 
-func _on_HSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
-
-
-func _on_FullScreen_pressed():
-	OS.window_fullscreen = !OS.window_fullscreen
-	
 func _on_Sair_pressed():
 	get_tree().quit()
 
+func _on_Config_pressed():
+	get_node("Sound").show()
+	AudioServer.generate_bus_layout()
 
+func _on_HSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
 
-func _on_Sound_pressed():
-	pass # Replace with function body.
+func _on_FullScreen2_pressed():
+	OS.window_fullscreen = !OS.window_fullscreen
