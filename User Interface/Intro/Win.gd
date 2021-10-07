9b854c637c9bdd6b6bf94fafe1c8b197978c400e
Andre <andreluizrodrigues14@gmail.com>
2021-10-06 21:54:13 -0400

    Adicionado crédito #1
    Adicionado crédito apenas no menu

--- User Interface/Intro/Win.gd
+++ User Interface/Intro/Win.gd
@@ -2,8 +2,6 @@ extends Control
 
 func _input(event):
 	yield( get_tree().create_timer(8.0), "timeout" )
-	get_node("Credito").show()
-
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://User Interface/Menu/Menu.tscn")
--- User Interface/Scripts/Menu.gd
+++ User Interface/Scripts/Menu.gd
@@ -9,22 +9,22 @@ func _input(event):
 	if !isPlaying:
 		get_node("MainTheme").play()
 		isPlaying = true
-		
-func _on_Button_pressed():
-# warning-ignore:return_value_discarded
+
+func _on_Jogar_pressed():
 	get_tree().change_scene("res://Levels/Maps/Level_01/LEVEL_Level_01.tscn")
 
+func _on_Button_pressed():
+	get_node("Credito").hide()
+# warning-ignore:return_value_discarded
+
 func _on_Config_pressed():
-	get_node("Sound").show()
-	AudioServer.generate_bus_layout()
-	 
+	get_node("Credito").show()
+ 
 func _on_Button3_pressed():
 	get_tree().change_scene("res://Levels/Maps/Level_00/LEVEL_Level_00.tscn")
 
-
 func _on_HSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
-
 
 func _on_FullScreen_pressed():
 	OS.window_fullscreen = !OS.window_fullscreen
