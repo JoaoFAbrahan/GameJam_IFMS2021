887bf971eeba8b46afd986eef8f590605fff1e83
Andre <andreluizrodrigues14@gmail.com>
2021-10-06 22:03:17 -0400

    Menu de aviso #1
    Implementado som de aviso

--- User Interface/AVISO/Code/CODE_SCENE_Aviso.gd
+++ User Interface/AVISO/Code/CODE_SCENE_Aviso.gd
@@ -1,8 +1,9 @@
 extends Control
 var timeIn = true;
 
-func _process(delta):
+func _input(event):
 	if timeIn:
 		$Timer.start();
 		yield($Timer,"timeout");
 		timeIn = false;
+	get_tree().change_scene("res://User Interface/Menu/Menu.tscn")
